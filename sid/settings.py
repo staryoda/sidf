06a8b8c1197d0ebd45133d44c961d30e39cee790
staryoda <hiiamsid518@gmail.com>
2021-03-22 15:14:34 +0530

    new

--- sid/settings.py
+++ sid/settings.py
@@ -131,7 +131,7 @@ LOGOUT_URL = 'logout'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'hackermannn2018@gmail.com'
-EMAIL_HOST_PASSWORD = 'activablack18'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 AUTHENTICATION_BACKENDS=['django.contrib.auth.backends.ModelBackend',
